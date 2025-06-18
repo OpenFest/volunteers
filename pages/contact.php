89bc315333a4aff6037f19c9f449064c48dd4b8f
Atanas Angelov <atanas.g.angelov@gmail.com>
2025-06-18 17:03:07 +0300

    Remove obsolete files

--- pages/contact.php
+++
@@ -1,8 +0,0 @@
-<?php
- $email = 'info@openfest.org';
- $phone = '359 2 123 4567';
-?>
-<h1>Contact Us</h1>
-<p>Feel free to reach out to us!</p>
-<p>Email: <?=htmlspecialchars($email)?></p>
-<p>Phone: <?= htmlspecialchars($phone)?></p>
