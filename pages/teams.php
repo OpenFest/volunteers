eec32b1d037392d68dd5ec85c3dd48524b5570bd
Atanas Angelov <atanas.g.angelov@gmail.com>
2025-06-20 17:30:42 +0300

    Teams page now uses name, instead of the slug

--- pages/teams.php
+++ pages/teams.php
@@ -1,12 +1,13 @@
 <?php
-$teams = $this->database->query("SELECT * FROM teams");
+$activeConf = 'of-2025';
+$teams = $this->database->query("SELECT * FROM teams WHERE conference = :conference", ['conference' => $activeConf]);
 ?>
 <h1>Екипи от доброволци</h1>
 
 <div class="teams-list">
 <?php foreach ($teams as $team) { ?>
     <div class="team-item">
-        <h2><?= htmlspecialchars($team->slug) ?></h2>
+        <h2><?= htmlspecialchars($team->name) ?></h2>
         <p><?= htmlspecialchars($team->description) ?></p>
     </div>
 <?php
