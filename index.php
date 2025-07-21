02280c2f1327de8172f914dcd0a4e919c0819282
Atanas Angelov <atanas.g.angelov@gmail.com>
2025-07-21 16:44:00 +0300

    Extra defines (debug + assets dir)

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
-//debug
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 //Front controller
 
 //import configuration
 require_once __DIR__ . '/config.php'; // Configuration file for a database and other settings
 require_once __DIR__ . '/helper.php'; // Helper functions for common tasks like dumping variables, etc.
 
+if (defined('DEBUG')) {
+	//debug
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+}
+
 // Define application paths
 define('BASE_PATH', '');  //keep empty if the app is in the web server root, or set to '/folder_name' if in a subdirectory
+define('ASSETS_DIR', __DIR__ . '/assets/');
 define('CORE_DIR', __DIR__ . '/core/');
 define('PAGES_DIR', __DIR__ . '/pages/');
 
