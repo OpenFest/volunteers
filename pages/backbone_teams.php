6add1d5f797ece689ef3d2fb4b18c3622581b39f
Atanas Angelov <atanas.g.angelov@gmail.com>
2026-09-11 12:19:20 +0300

    Teams filter, based on conference

--- pages/backbone_teams.php
+++ pages/backbone_teams.php
@@ -2,13 +2,43 @@
 
 checkAdmin();
 
+//get conf from the url parmas
+$allConferences = Conference::getConferences();
+$activeConference = Conference::getActive();
+$_conf = $_REQUEST['conf'] ?? '';
+
+$conference = null;
+if (empty($_conf)) {
+    if ($activeConference) {
+        $conference = $activeConference->toObject();
+    }
+} else {
+    foreach ($allConferences as $conf) {
+        if ($conf->slug === $_conf) {
+            $conference = $conf;
+            break;
+        }
+    }
+}
+
 $teams = $this->database->query(
-	'SELECT * FROM teams ORDER BY conference DESC'
+	'SELECT * FROM teams WHERE conference = COALESCE(:conference, conference) ORDER BY conference DESC',
+	[':conference' => $conference ? $conference->slug : null]
 );
 ?>
 <div class="backbone-page">
 	<div class="page-title">
-		<h1>Teams</h1>
+        <h1>Teams</h1>
+        <h3>
+            <label for="conference-select">Conference: </label><select name="conference" id="conference-select" onchange="window.location.href='/backbone/teams?conf=' + this.value">
+                <option value="all">All Conferences</option>
+                <?php foreach ($allConferences as $conf): ?>
+                    <option value="<?php echo htmlspecialchars($conf->slug); ?>" <?php echo ($conference && $conference->slug === $conf->slug) ? 'selected' : ''; ?>>
+                        <?php echo htmlspecialchars($conf->title); ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+        </h3>
 	</div>
 
 	<div class="pane full-width">
