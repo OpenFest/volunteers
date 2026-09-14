8dd1e2953ada75217f9121e6857fccac45df1323
Atanas Angelov <atanas.g.angelov@gmail.com>
2026-09-14 10:12:25 +0300

    Verification sends mail to core now

--- pages/verify.php
+++ pages/verify.php
@@ -24,12 +24,56 @@ function verify($database): User|bool|null
     $user = User::load($user);
 
     $user->resetToken();
-//verify non-verified volunteers linked to this user
+//verify non-verified volunteers linked to this user for the currently active conference
+
+    $activeConference = Conference::getActive();
+    if (!$activeConference) {
+        echo "<h3 class='login-error'>Няма активна конференция.</h3>";
+        return FALSE;
+    }
     $database->query(
-            'UPDATE volunteers SET verified = TRUE WHERE "user" = :uid AND verified = FALSE',
-            [':uid' => $user->getId()]
+            'UPDATE volunteers SET verified = TRUE WHERE "user" = :uid AND verified = FALSE AND conference = :conference',
+            [':uid' => $user->getId(), ':conference' => $activeConference->getSlug()]
+    );
+    _log("User {$user->getId()} verified for conference {$activeConference->getSlug()}");
+
+    $volunteers = $database->query(
+            'SELECT v.*, vt.conference, c.title, json_object_agg(t.name, vt.is_primary) as teams FROM volunteers v 
+    LEFT JOIN volunteer_teams vt ON v.id=vt.volunteer
+    LEFT JOIN teams t ON vt.team = t.slug 
+    LEFT JOIN conferences c ON vt.conference = c.slug
+    WHERE v.user = :uid AND vt.conference = :conference group by v.id, vt.conference, v.registration_date, c.title ORDER BY v.registration_date DESC',
+            [':uid' => $user->getId(), ':conference' => $activeConference->getSlug()]
     );
 
+
+    foreach ($volunteers as $volunteer) {
+        //send mail to core about the verification
+        $subject = "Нов доброволец за {$activeConference->getTitle()}";
+        $message = <<<EOT
+Здравейте,
+
+Потребителят {$user->getName()} <{$user->getEmail()}> потвърди регистрацията си като доброволец.
+
+Екипи: {$volunteer->teams}
+
+Език: {$volunteer->language}
+Телефон: {$volunteer->phone}
+Размер на тениска: {$volunteer->tshirt_size}
+Кройка на тениска: {$volunteer->tshirt_cut}
+Предпочитания за храна: {$volunteer->food_preferences}
+
+Предходен опит:
+{$volunteer->previous_experience}
+EOT;
+
+        $headers = "From:no-reply@openfest.org\r\nReply-To:no-reply@openfest.org\r\n";
+        $coreEmail = 'core@openfest.org';
+        if (!mail($coreEmail, $subject, $message, $headers)) {
+            _log("Failed to send verification notification email to core: " . $coreEmail, LOG_ERR);
+        }
+        _log("Sent verification notification email to core: " . $coreEmail);
+    }
     return $user;
 }
 
