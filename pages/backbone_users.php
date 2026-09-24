ff6f457d40be38c1b3c3d180b71b0479f7cec1fe
Atanas Angelov <atanas.g.angelov@gmail.com>
2026-09-24 15:16:40 +0300

    New and Existing users split in the list

--- pages/backbone_users.php
+++ pages/backbone_users.php
@@ -2,14 +2,17 @@
 
 checkAdmin();
 
-$users = $this->database->query(
-	'SELECT * FROM users ORDER BY name ASC'
+$newUsers = $this->database->query(
+	'SELECT * FROM users WHERE username is NULL ORDER BY name ASC'
+);
+$existingUsers = $this->database->query(
+    'SELECT * FROM users WHERE username is NOT NULL ORDER BY name ASC'
 );
 
 ?>
 <div class="backbone-page">
     <div class="page-title">
-        <h1>Users</h1>
+        <h1>New Users</h1>
     </div>
 
 	<div class="pane full-width">
@@ -26,7 +29,7 @@ $users = $this->database->query(
 				</tr>
 			</thead>
 			<tbody>
-				<?php foreach ($users as $user): ?>
+				<?php foreach ($newUsers as $user): ?>
 					<tr>
                         <td><a href="/backbone/profile?user=<?php echo $user->uid; ?>"><?php echo htmlspecialchars($user->username ?? 'n/a'); ?></a></td>
                         <td><?php echo htmlspecialchars($user->name); ?></td>
@@ -38,4 +41,35 @@ $users = $this->database->query(
 				<?php endforeach; ?>
 		</table>
 	</div>
+
+	<div class="page-title">
+        <h1>Existing Users</h1>
+    </div>
+    <div class="pane full-width">
+        <table>
+            <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Admin</th>
+                    <th>Active</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($existingUsers as $user): ?>
+                    <tr>
+                        <td><a href="/backbone/profile?user=<?php echo $user->uid; ?>"><?php echo htmlspecialchars($user->username ?? 'n/a'); ?></a></td>
+                        <td><?php echo htmlspecialchars($user->name); ?></td>
+                        <td><?php echo htmlspecialchars($user->email); ?></td>
+                        <td><?php echo htmlspecialchars($user->phone); ?></td>
+                        <td><?php echo $user->admin ? '<span class="green">✔</span>' : '<span class="red">✘</span>';?></td>
+                        <td><?php echo $user->active ? '<span class="green">✔</span>' : '<span class="red">✘</span> <a href="/backbone/user/activate?user=' . $user->uid .'" class="button green">Activate</a>';?></td>
+                    </tr>
+                <?php endforeach; ?>
+        </table>
+    </div>
 </div>
+
